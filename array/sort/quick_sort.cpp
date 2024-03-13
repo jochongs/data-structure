8ac8492b793695801e47f6e38de84d0069c42e0c
jochongs <joch2712@naver.com>
2024-03-13 16:45:46 +0900

    feat: add direction in quick_sort

--- array/sort/quick_sort.cpp
+++ array/sort/quick_sort.cpp
@@ -11,14 +11,17 @@ int select_pivot_idx(int arr[], int start_idx, int end_idx) {
     return end_idx;
 }
 
-int partition(int arr[], int start_idx, int end_idx) {
+int partition(int arr[], int start_idx, int end_idx, int direction = 1) {
     int pivot_idx = select_pivot_idx(arr, start_idx, end_idx);
     int pivot = arr[pivot_idx];
 
     int i = start_idx - 1;
 
     for (int j = start_idx; j <= end_idx; j++) {
-        if (arr[j] < pivot) {
+        if (
+            direction == 1 && arr[j] < pivot || 
+            direction != 1 && arr[j] > pivot
+        ) {
             i++;
             swap(arr, i, j);
         }
@@ -28,15 +31,19 @@ int partition(int arr[], int start_idx, int end_idx) {
     return i + 1;
 }
 
-void quick_sort(int arr[], int start_idx, int end_idx) {
+void quick_sort(int arr[], int start_idx, int end_idx, int direction = 1) {
     if (start_idx >= end_idx) {
         return;
     }
 
-    int mid_idx = partition(arr, start_idx, end_idx);
+    int mid_idx = partition(arr, start_idx, end_idx, direction);
 
-    quick_sort(arr, start_idx, mid_idx - 1);
-    quick_sort(arr, mid_idx + 1, end_idx);
+    quick_sort(arr, start_idx, mid_idx - 1, direction);
+    quick_sort(arr, mid_idx + 1, end_idx, direction);
+}
+
+void quick_sort(int arr[], int n, int direction = 1) {
+    quick_sort(arr, 0, n - 1, 1);
 }
 
 int main() {
@@ -49,7 +56,7 @@ int main() {
         cin >> arr[i];
     }
 
-    quick_sort(arr, 0, n - 1);
+    quick_sort(arr, n);
 
     for (int i = 0; i < n; i++) {
         cout << arr[i] << ' ';
