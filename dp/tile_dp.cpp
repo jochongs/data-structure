c971b21515c127ee7a5339bf49d301b7d5e245a1
jochongs <joch2712@naver.com>
2024-03-21 11:48:40 +0900

    feat: add tile_count_2

--- dp/tile_dp.cpp
+++ dp/tile_dp.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+long long tile_count_2(int n, long long memo[]) {
+    if (n <= 1) {
+        return 1;
+    }
+    
+    if (memo[n] != 0) {
+        return memo[n];
+    }
+
+    memo[n] = tile_count_2(n - 1, memo) + tile_count_2(n - 2, memo) + tile_count_2(n - 2, memo);
+    return memo[n];
+}
+
 long long tile_count(int n, long long memo[]) {
     if (n <= 1) {
         memo[n] = 1;
@@ -21,7 +34,7 @@ int main() {
 
     long long memo[10000] = {0, };
 
-    long long tile_num = tile_count(n, memo);
+    long long tile_num = tile_count_2(n, memo);
 
     cout << "Tile Count: " << tile_num << '\n';
 }
